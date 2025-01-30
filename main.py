632f4f5b3881423fc9450e074d9588ca8600cbcf
janjbyte <jason.nevajuoni@gmail.com>
2025-01-30 22:53:01 +0100

    Count characters

--- main.py
+++ main.py
@@ -4,6 +4,26 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"I've found {num_words} words in the book")
+    lower_case = characters(text)
+    counts = get_characters(lower_case)
+    print(counts)
+
+# Skapade två funktioner för att först ändra till små bokstäver, därefter för att beräkna i dictionary
+def characters(text):
+    text_lower = text.lower()
+    character = list(text_lower)
+    return character
+
+
+def get_characters(lower_case):
+    counts = {}
+    for chars in lower_case:
+        if chars in counts:
+            counts[chars] += 1
+        else:
+            counts[chars] = 1
+    return counts
+    
 
 # Skapar funktion för att räkna    
 def get_num_words(text):
